7a12d3e591413c18e39e420e349f902b0e815420
nandiswarb93 <nandiswarb93@gmail.com>
2026-04-10 15:31:45 +0530

    code fixed

--- KINE/wsgi.py
+++ KINE/wsgi.py
@@ -1,14 +1,20 @@
-"""
-WSGI config for KINE project.
+# """
+# WSGI config for KINE project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+# It exposes the WSGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
+# For more information on this file, see
+# https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+# """
 
+# import os
+
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'KINE.settings')
+
+# application = get_wsgi_application()
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'KINE.settings')
